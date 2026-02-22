3b359eda41446595a7b051f8b9ee188fd6c3c35e
lucasgzzm <165522078+lucasgzzm@users.noreply.github.com>
2026-02-22 17:49:57 -0300

    act

--- sw.js
+++ sw.js
@@ -1,30 +1,24 @@
-const CACHE_NAME = "saldosmart-v1";
-const FILES_TO_CACHE = [
+const CACHE_NAME = "saldosmart-v2";
+
+const FILES = [
   "./",
   "./index.html",
   "./add.html",
-  "./history.html",
+  "./styles.css",
   "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./icon-192.png"
 ];
 
-self.addEventListener("install", (e) => {
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache=>cache.addAll(FILES))
   );
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-});
-
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res=>res || fetch(e.request))
   );
 });
